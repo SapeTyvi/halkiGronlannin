1479fc6931acfd7e55e2aec99b97d844aacfa22a
lakda <lakda20@gmail.com>
2017-09-26 17:56:47 +0300

    lisäys
    header + footer

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+
+</head>
+
+<body>
--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
-    </body>
-</html>
+
+<?php get_header(); ?>
+
+
+<?php get_footer(); ?>
