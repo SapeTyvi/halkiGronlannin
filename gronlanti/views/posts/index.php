18043d5b0ec47dae77a266212b2c05f5bed1711b
SapeTyvi <32194937+SapeTyvi@users.noreply.github.com>
2017-10-09 15:24:47 +0300

    Add files via upload

--- gronlanti/views/posts/index.php
+++ gronlanti/views/posts/index.php
@@ -3,7 +3,7 @@
 
 <?php foreach($posts as $post) { ?>
 	<p>
-		<?php echo $post->$subject; ?>
-		<a href='?controller=posts&action=show&id=<?php echo $post->id; ?>'>Katso sisältö</a>
+		<?php echo $post->subject; ?>
+		<a href='?controller=posts&action=showPosts&blogId=<?php echo $post->blogId; ?>'>Katso sisältö</a>
 	</p>
 <?php } ?>
