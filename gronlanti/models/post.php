e557397d9436921cd145d663c0dd0d6e3369c956
SapeTyvi <32194937+SapeTyvi@users.noreply.github.com>
2017-10-09 15:23:48 +0300

    Add files via upload

--- gronlanti/models/post.php
+++ gronlanti/models/post.php
@@ -1,33 +1,33 @@
 <?php 
 	class Post {
-		public $id;
+		public $blogId;
 		public $subject;
 		public $content;
 		
-		public function __construct($id, $subject, $content){
-			$this->id		= $id;
-			$this->subject	= $subject;
-			$this->content	= $content;
+		public function __construct($blogId, $subject, $content){
+			$this->blogId		= $blogId;
+			$this->subject		= $subject;
+			$this->content		= $content;
 		}
 		
 		public static function all(){
 			$list	= [];
-			$db		= testDb::getInstance();
+			$db		= testDB::getInstance();
 			$req 	= $db->query('SELECT * FROM blogposts');
 			
 			foreach($req->fetchAll() as $post){
-				$list[] = new Post($post['id'], $post['subject'], $post['content']);
+				$list[] = new Post($post['blogId'], $post['subject'], $post['content']);
 			}
 			return $list;
 		}
-		public static function find($id){
-			$db = testDb::getInstance();
-			$id = intval($id);
-			$req = $db->prepare('SELECT * FROM blogposts WHERE id = :id');
-			$req->execute(array('id' => $id));
+		public static function find($blogId){
+			$db = testDB::getInstance();
+			$blogId = intval($blogId);
+			$req = $db->prepare('SELECT * FROM blogposts WHERE blogId = :blogId');
+			$req->execute(array('blogId' => $blogId));
 			$post = $req->fetch();
 			
-			return new Post($post['id'], $post['subject'], $post['content']);
+			return new Post($post['blogId'], $post['subject'], $post['content']);
 		}
 	}
 ?>
