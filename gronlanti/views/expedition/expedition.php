7d511f69552ede37727b5f5ac616152bb4675a66
Ricardo Oksa <riku_oksa_91@hotmail.com>
2017-10-10 11:47:08 +0300

    Delete expedition.php

--- gronlanti/views/expedition/expedition.php
+++
@@ -1,8 +0,0 @@
-<p>Retkikunta</p>
-
-<?php foreach($expeditors as $expeditor) { ?>
-	<p>
-		<?php echo $expeditor->firstName; ?>
-		<a href='?controller=expedition&action=showExpeditors&pId=<?php echo $expeditor->pId; ?>'>Lue lisää</a>
-	</p>
-<?php } ?>
