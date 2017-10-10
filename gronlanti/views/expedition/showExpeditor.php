86fe7564b0ee47636ec87d7ca91090848554db0b
Ricardo Oksa <riku_oksa_91@hotmail.com>
2017-10-10 11:46:49 +0300

    Delete showExpeditor.php

--- gronlanti/views/expedition/showExpeditor.php
+++
@@ -1,7 +0,0 @@
-<p>Tässä Lisätiedot:</p>
-
-<p><?php echo $expeditor->firstName; ?></p>
-<p><?php echo $expeditor->lastName; ?></p>
-<p><?php echo $expeditor->occupation; ?></p>
-<p><?php echo $expeditor->introduction; ?></p>
-<p><?php# echo $expeditor->picture; ?></p>
