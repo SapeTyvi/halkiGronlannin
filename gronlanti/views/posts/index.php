120f3f6b930c15170e4f2b66a9022b2817ee4afa
Ricardo Oksa <riku_oksa_91@hotmail.com>
2017-10-10 11:46:14 +0300

    Delete index.php

--- gronlanti/views/posts/index.php
+++
@@ -1,9 +0,0 @@
-
-<p>Tässä kaikki blogipostaukset:</p>
-
-<?php foreach($posts as $post) { ?>
-	<p>
-		<?php echo $post->subject; ?>
-		<a href='?controller=posts&action=showPosts&blogId=<?php echo $post->blogId; ?>'>Katso sisältö</a>
-	</p>
-<?php } ?>
